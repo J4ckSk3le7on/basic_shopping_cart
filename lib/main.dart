2e718d89b4a9ae5aff7f0b9475958dabcb8291a0
Jorge Greñas <j4ck.devcode@gmail.com>
2021-02-09 17:09:46 -0500

    Firebase Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
 
-void main() {
+import 'home_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Basic Shopping',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      initialRoute: "home_page",
+      routes: {
+        "home"  : (BuildContext context) => HomePage(),
+      },
     );
   }
 }
